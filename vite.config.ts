fd3c78490667a48922c5f60f8a17194a6939ba10
cl4sh3r <138099022+cl4sh3r@users.noreply.github.com>
2025-12-18 14:01:48 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/catur-with-kids/",
+  base: "/catur-anak/",
   plugins: [react()],
   resolve: {
     alias: {
